dee858c4b0f4db0a791e0e8a7ec0b640c1787906
Stephen Zhu <I067561@pvgl50801824a.apj.global.corp.sap>
2017-09-19 12:12:13 +0800

    enable oauth2

--- src/main/java/org/rvslab/chapter2/Application.java
+++ src/main/java/org/rvslab/chapter2/Application.java
@@ -2,9 +2,11 @@ package org.rvslab.chapter2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableGlobalMethodSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
 @SpringBootApplication
 public class Application {
 
--- src/test/java/org/rvslab/chapter2/ApplicationTests.java
+++ src/test/java/org/rvslab/chapter2/ApplicationTests.java
@@ -2,6 +2,7 @@ package org.rvslab.chapter2;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class ApplicationTests {
     @Autowired
     private TestRestTemplate restTemplate;
@@ -25,13 +29,15 @@ public class ApplicationTests {
 	public void contextLoads() {
 	}
 	
+	@Ignore
 	@Test
 	public void testVanillaService() {
-		Greet greet = restTemplate.getForObject("/Hello", Greet.class);
+		Greet greet = restTemplate.getForObject("http://localhost:8080/Hello", Greet.class);
 		
 		assertEquals("Full authentication is required to access this resource", greet.getMessage());
 	}
 	
+	@Ignore
 	@Test
 	public void testSecureService() {
 		String plainCreds = "guest:guest123";
@@ -42,6 +48,24 @@ public class ApplicationTests {
 		
 		ResponseEntity<Greet> response = restTemplate.exchange("/Hello", HttpMethod.GET, request, Greet.class);
 		assertEquals("Hello HATEOAS", response.getBody().getMessage());
+	}
+	
+	@Test
+	public void testOAuthService() {
+		ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+		resource.setUsername("guest");
+		resource.setPassword("guest123");
+		resource.setAccessTokenUri("http://localhost:8080/oauth/token");
+		
+		resource.setClientId("trustedclient");
+		resource.setClientSecret("trustedclient123");
+		resource.setGrantType("password");
+		
+		DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+		OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
+		
+		Greet greet = restTemplate.getForObject("http://localhost:8080/Hello", Greet.class);
+		assertEquals("Hello HATEOAS", greet.getMessage());
 	}
 
 }

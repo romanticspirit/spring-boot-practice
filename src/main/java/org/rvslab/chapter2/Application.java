8437ffa76d028abd3ed7db4fcf773bad023946e2
Stephen Zhu <I067561@pvgl50801824a.apj.global.corp.sap>
2017-09-19 11:22:39 +0800

    add security

--- src/main/java/org/rvslab/chapter2/Application.java
+++ src/main/java/org/rvslab/chapter2/Application.java
@@ -2,7 +2,9 @@ package org.rvslab.chapter2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@EnableGlobalMethodSecurity
 @SpringBootApplication
 public class Application {
 
--- src/test/java/org/rvslab/chapter2/ApplicationTests.java
+++ src/test/java/org/rvslab/chapter2/ApplicationTests.java
@@ -7,6 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
@@ -24,7 +29,19 @@ public class ApplicationTests {
 	public void testVanillaService() {
 		Greet greet = restTemplate.getForObject("/Hello", Greet.class);
 		
-		assertEquals("Hello HATEOAS", greet.getMessage());
+		assertEquals("Full authentication is required to access this resource", greet.getMessage());
+	}
+	
+	@Test
+	public void testSecureService() {
+		String plainCreds = "guest:guest123";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + new String(Base64.encode(plainCreds.getBytes())));
+		
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		
+		ResponseEntity<Greet> response = restTemplate.exchange("/Hello", HttpMethod.GET, request, Greet.class);
+		assertEquals("Hello HATEOAS", response.getBody().getMessage());
 	}
 
 }

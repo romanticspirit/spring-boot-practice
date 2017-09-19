3f4438623558282b401d3f34f10ea35f6bbb201e
Stephen Zhu <I067561@pvgl50801824a.apj.global.corp.sap>
2017-09-19 12:14:20 +0800

    cross origin

--- src/main/java/org/rvslab/chapter2/GreetingController.java
+++ src/main/java/org/rvslab/chapter2/GreetingController.java
@@ -4,6 +4,7 @@ import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 	@RequestMapping("/Hello")
+	@CrossOrigin
 	HttpEntity<Greet> greeting(@RequestParam(value="name", required=false, defaultValue = "HATEOAS")String name) {
 		Greet greet = new Greet("Hello " + name);
 		greet.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(GreetingController.class).greeting(name)).withSelfRel());
